294d8d0c2bc43bbf882bdf3ae17b0873135a17ce
Matiasrv <matuterv@hotmail.com>
2016-10-28 13:10:45 -0300

    Dos jugadores con red sin pelotas
    Pelotas de volley

--- source/sprites/Jugador.hx
+++ source/sprites/Jugador.hx
@@ -3,23 +3,43 @@ package sprites;
 import flixel.FlxSprite;
 import flixel.system.FlxAssets.FlxGraphicAsset;
 import flixel.FlxG;
+import flixel.FlxObject;
 /**
  * ...
  * @author ...
  */
 class Jugador extends FlxSprite
 {
-
+	private var speed:Float = 225;
+	private var numeroJugador:Bool = true;
+	
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
 		super(X, Y, SimpleGraphic);
-		makeGraphic(16, 32);
+		makeGraphic(32, 64);
+		acceleration.y = 800;
 	}
 	override public function update(elapsed:Float):Void
 	{
 		velocity.x = 0;
-		if (FlxG.keys.pressed.A)
-			velocity.x 
+		switch(numeroJugador)
+		{
+			case true:	
+				if (FlxG.keys.pressed.A && x>0)
+					velocity.x = -speed;
+				if (FlxG.keys.pressed.D)
+					velocity.x = speed;
+			default:
+				if (FlxG.keys.pressed.J)
+					velocity.x = -speed;
+				if (FlxG.keys.pressed.L && x<FlxG.width-width)
+					velocity.x = speed;
+		}
+			
 		super.update(elapsed);
+	}
+	public function setearTeclas(a:Bool)
+	{
+		numeroJugador = a;
 	}
 }
--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -6,16 +6,41 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
+import sprites.Jugador;
+import flixel.FlxObject;
 
 class PlayState extends FlxState
 {
+	private var piso:FlxSprite;
+	private var player1:Jugador;
+	private var player2:Jugador;
+	private var net:FlxSprite = new FlxSprite(FlxG.width/2 -1 , 240);
+	
 	override public function create():Void
 	{
 		super.create();
+		net.makeGraphic(3, 240);
+		player1 = new Jugador(FlxG.width/4, 50);
+		player2 = new Jugador(FlxG.width / 2 + FlxG.width / 4, 50);
+		player2.setearTeclas(false);
+		piso = new FlxSprite(0,FlxG.height-20);
+		piso.makeGraphic(FlxG.width, 20);
+		piso.immovable = true;
+		add(player1);
+		add(player2);
+		add(piso);
+		add(net);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		FlxG.collide(player1, piso);
+		FlxG.collide(player2, piso);
+		if (player1.x >= FlxG.width/2 - player1.width)
+			player1.x = FlxG.width/2 - player1.width;
+		if (player2.x <= FlxG.width/2)
+			player2.x = FlxG.width/2;
+		
 	}
 }
