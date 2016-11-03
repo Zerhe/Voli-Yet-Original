15e462787f88e5abc2ea7b565517544f397dd96d
Zerhe <dante_ro@hotmail.com.ar>
2016-11-02 21:33:39 -0300

    La barra xDxd

--- source/sprites/Jugador.hx
+++ source/sprites/Jugador.hx
@@ -13,29 +13,43 @@ class Jugador extends FlxSprite
 	private var speed:Float = 225;
 	private var numeroJugador:Bool = true;
 	public var barraEnergia:FlxSprite;
+	private var barraLife:Int = 100;
+	private var timerBarra:Int = 0;
 	
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
 		super(X, Y, SimpleGraphic);
 		makeGraphic(32, 64);
 		acceleration.y = 800;
-		barraEnergia = new FlxSprite(16, 100);
+		barraEnergia = new FlxSprite(16, 50);
+		barraEnergia.makeGraphic(16, 100, 0xffff0000);
+		barraEnergia.alpha = 0.5;
 	}
 	override public function update(elapsed:Float):Void
 	{
 		velocity.x = 0;
+		timerBarra++;
+		if (barraLife < 100 && timerBarra == 10){
+			barraLife++;
+			timerBarra = 0;
+		}
+		trace(barraLife);
+		trace(timerBarra);
 		switch(numeroJugador)
 		{
-			case true:	
+			case true:
 				if (FlxG.keys.pressed.A && x>0)
 					velocity.x = -speed;
 				if (FlxG.keys.pressed.D)
 					velocity.x = speed;
 				if (FlxG.keys.justPressed.W && isTouching(FlxObject.FLOOR))
 					velocity.y = -450;
-				if (FlxG.keys.pressed.W && !isTouching(FlxObject.FLOOR))
+				if (FlxG.keys.pressed.W && !isTouching(FlxObject.FLOOR) && barraLife > 0){
 					velocity.y = -200;
+					barraLife -= 5;
+				}
 			default:
+				barraEnergia.x = FlxG.width - barraEnergia.width * 2;
 				if (FlxG.keys.pressed.J)
 					velocity.x = -speed;
 				if (FlxG.keys.pressed.L && x<FlxG.width-width)
