c55024d36ad9a857aee6e56b25b3b0c4a14503ee
Zerhe <dante_ro@hotmail.com.ar>
2016-11-09 21:52:53 -0300

    pILIDO XdXds
    :3

--- source/sprites/Jugador.hx
+++ source/sprites/Jugador.hx
@@ -12,8 +12,7 @@ class Jugador extends FlxSprite
 {
 	private var hSpeed:Float = 225;
 	private var numeroJugador:Bool = true;
-	public var barraEnergia:FlxSprite;
-	private var barraLife:Int = 1000;
+	private var barraLife:Float = 100;
 	private var timerBarra:Int = 0;
 	
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
@@ -21,51 +20,47 @@ class Jugador extends FlxSprite
 		super(X, Y, SimpleGraphic);
 		makeGraphic(32, 64);
 		acceleration.y = 800;
-		barraEnergia = new FlxSprite(16, 50);
-		barraEnergia.makeGraphic(16, 100, 0xffff0000);
-		barraEnergia.alpha = 0.5;
 	}
 	override public function update(elapsed:Float):Void
 	{
 		velocity.x = 0;
-		timerBarra++;
-		trace(barraLife);
-		trace(timerBarra);
+		if(barraLife < 100)
+			timerBarra++;
+		//trace(barraLife);
+		//trace(timerBarra);
 		
-		if (timerBarra == 100){
-			barraLife+= 1000;
-			timerBarra = 0;
+		if (timerBarra >= 100){
+			if(isTouching(FlxObject.FLOOR)){
+				barraLife+= 100;
+				timerBarra = 0;
+			}
 		}
-		if (barraLife > 1000)
-			barraLife = 1000;
-		/*for (i in 0...100)
-			if (barraLife == i * 10)
-				barraEnergia.makeGraphic(16, i, 0xffff0000);
-		*/
+		if (barraLife > 100)
+			barraLife = 100;
 		switch(numeroJugador)
 		{
 			case true:
-				if (FlxG.keys.pressed.A && x>0)
-					velocity.x = -hSpeed;
-				if (FlxG.keys.pressed.D)
+				if (FlxG.keys.pressed.D && x <= FlxG.width/2 - width - 2)
 					velocity.x = hSpeed;
+				else if (FlxG.keys.pressed.A && x>0)
+					velocity.x = -hSpeed;
 				if (FlxG.keys.justPressed.W && isTouching(FlxObject.FLOOR))
 					velocity.y = -450;
-				if (FlxG.keys.pressed.W && !isTouching(FlxObject.FLOOR) && barraLife > 59){
+				if (FlxG.keys.pressed.W && !isTouching(FlxObject.FLOOR) && barraLife > 5.9){
 					velocity.y = -200;
-					barraLife -= 25;
+					barraLife -= 1.5;
 				}
 			default:
-				barraEnergia.x = FlxG.width - barraEnergia.width * 2;
-				if (FlxG.keys.pressed.LEFT)
+				//barraEnergia.x = FlxG.width - barraEnergia.width * 2;
+				if (FlxG.keys.pressed.LEFT && x >= FlxG.width/2 + 2)
 					velocity.x = -hSpeed;
 				if (FlxG.keys.pressed.RIGHT && x<FlxG.width-width)
 					velocity.x = hSpeed;
 				if (FlxG.keys.justPressed.UP && isTouching(FlxObject.FLOOR))
 					velocity.y = -450;
-				if (FlxG.keys.pressed.UP && !isTouching(FlxObject.FLOOR) && barraLife > 59){
+				if (FlxG.keys.pressed.UP && !isTouching(FlxObject.FLOOR) && barraLife > 5.9){
 					velocity.y = -200;
-					barraLife -= 25;
+					barraLife -= 1.5;
 				}
 		}
 		super.update(elapsed);
--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -3,6 +3,7 @@ package states;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.ui.FlxBar;
 import flixel.text.FlxText;
 import sprites.Jugador;
 import flixel.FlxObject;
@@ -13,19 +14,18 @@ class PlayState extends FlxState
 	private var piso:FlxSprite;
 	private var player1:Jugador;
 	private var player2:Jugador;
-	private var net:FlxSprite = new FlxSprite(FlxG.width/2, 240);
+	private var net:FlxSprite = new FlxSprite(FlxG.width/2-1, 260);
 	private var pelota:Pelota;
-	//private var score01:Int = 0;
-	//private var score02:Int = 0;
 	private var scoreText:FlxText;
-	
-
+	private var energiaPlayer1:FlxBar;
+	private var energiaPlayer2:FlxBar;
 	
 	override public function create():Void
 	{
 		super.create();
 		net.makeGraphic(3, 240);
-		scoreText = new FlxText(300, 20, 0, Reg.score01 + "|" + Reg.score02, 20);
+		FlxG.mouse.visible = false;
+		scoreText = new FlxText(300, 20, 0, Reg.score01 + "  |  " + Reg.score02, 20);
 		player1 = new Jugador(FlxG.width/4, 50);
 		player2 = new Jugador(FlxG.width / 2 + FlxG.width / 4, 50);
 		player2.setearTeclas(false);
@@ -33,32 +33,42 @@ class PlayState extends FlxState
 		piso.makeGraphic(FlxG.width, 20);
 		piso.immovable = true;
 		net.immovable = true;
-		pelota = new Pelota(FlxG.width/2-16, 100);
+		pelota = new Pelota(FlxG.width / 2 - 16, 100);
+		energiaPlayer1 = new FlxBar(10, 10);
+		energiaPlayer2 = new FlxBar(10, 10);
+		energiaPlayer2.flipX = true;
+		energiaPlayer2.x = (FlxG.width - energiaPlayer2.width - 10);
+		energiaPlayer1.parent = player1;
+		energiaPlayer2.parent = player2;
+		energiaPlayer1.parentVariable = "barraLife";
+		energiaPlayer2.parentVariable = "barraLife";
+		add(energiaPlayer1);
+		add(energiaPlayer2);
 		add(piso);
 		add(scoreText);
 		add(pelota);
+		add(net);
 		add(pelota.sombra);
 		add(player1);
-		add(player1.barraEnergia);
 		add(player2);
-		add(player2.barraEnergia);
-		add(net);
+		add(energiaPlayer1);
+		add(energiaPlayer2);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
 		scoreText.destroy();
-		scoreText = new FlxText(300, 20, 0, Reg.score01 + " | " +Reg.score02, 20);
+		scoreText = new FlxText(FlxG.width/2, 20, 0, Reg.score01 + " | " +Reg.score02, 20);
+		scoreText.x -= scoreText.width / 2;
 		add(scoreText);
 		FlxG.collide(player1, piso);
 		FlxG.collide(player2, piso);
 		colisionesPelota();
-		if (player1.x >= FlxG.width/2 - player1.width)
-			player1.x = FlxG.width/2 - player1.width;
-		if (player2.x <= FlxG.width/2)
-			player2.x = FlxG.width/2;
-		
+		if (player1.y + player1.height > piso.y)
+			player1.y = piso.y - player1.height;
+		if (player2.y + player2.height > piso.y)
+			player2.y = piso.y - player2.height;
 		if (FlxG.keys.pressed.R)
 			FlxG.resetState();
 	}
@@ -75,14 +85,17 @@ class PlayState extends FlxState
 			pelota.velocity.x = 0;*/
 			FlxG.resetState();
 		}
-		if(FlxG.collide(player1, pelota))
+		if (FlxG.collide(player1, pelota) && player1.y > pelota.y - player1.height/2)
 			pelota.velocity.y = -350;
-		if(FlxG.collide(player2, pelota))
+		if (FlxG.collide(player2, pelota) && player2.y > pelota.y - player2.height/2)
 			pelota.velocity.y = -350;
 		if (FlxG.collide(pelota, net))
 		{
 			pelota.velocity.y = -200;
-			pelota.velocity.x *= -1;
+			if (pelota.x < net.x - pelota.width + 1)
+				pelota.velocity.x = -150;
+			else if (pelota.x > net.x)
+				pelota.velocity.x = 150;
 		}
 	}
 }
